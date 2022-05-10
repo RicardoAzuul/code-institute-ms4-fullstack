f026d8fc2b4ebe114bb98d8670d388a593c5f654
Richard Blaauw <richard.blaauw@ogd.nl>
2022-05-10 21:00:33 +0200

    Fix environment settings in settings.py: Heroku was unable to build due to env settings not being read properly

--- fit_family/settings.py
+++ fit_family/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', '')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in env
+DEBUG = env('DEVELOPMENT')
 
 ALLOWED_HOSTS = ['code-institute-ms4.herokuapp.com', 'localhost']
 
@@ -200,11 +200,11 @@ if 'USE_AWS' in os.environ:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STRIPE_CURRENCY = 'usd'
-STRIPE_PUBLIC_KEY = env('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY', '')
-STRIPE_WH_SECRET = env('STRIPE_WH_SECRET', '')
+STRIPE_PUBLIC_KEY = env('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
+STRIPE_WH_SECRET = env('STRIPE_WH_SECRET')
 
-if 'DEVELOPMENT' in env:
+if env('DEVELOPMENT'):
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 else:
