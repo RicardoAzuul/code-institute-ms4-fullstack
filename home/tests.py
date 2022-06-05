b388e2c380332be3cdff86eb9200d0764154622d
Richard Blaauw <richard.blaauw@ogd.nl>
2022-06-05 21:29:03 +0200

    Apply CSS to a button that was overlooked, add some basic view tests

--- home/tests.py
+++ home/tests.py
@@ -7,3 +7,19 @@ class TestViews(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/index.html')
+
+    def test_get_signup(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/signup.html')
+
+    def test_get_login(self):
+        response = self.client.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/login.html')
+
+    def test_get_password_reset(self):
+        response = self.client.get('/accounts/password/reset/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/password_reset.html')        
+    
