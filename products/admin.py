6b9a73a6dca4e8864666aba1d24561984e1dfac6
Richard Blaauw <richard.blaauw@ogd.nl>
2022-06-10 20:59:54 +0200

    Customize admin portal: show all the properties of the Product and Product_Category models

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Product_Category, Product
 
 # Register your models here.
-admin.site.register(Product_Category)
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'sku',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('name',)
+
+class Product_CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+    ordering = ('friendly_name',)
+
+admin.site.register(Product_Category, Product_CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
