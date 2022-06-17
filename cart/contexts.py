02ca5c225a8fc1842443de52716f72711fed6c9b
Richard Blaauw <richard.blaauw@ogd.nl>
2022-06-17 15:26:31 +0200

    Add context processor for the cart app, add CSS to the cart template

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,8 @@
 from decimal import Decimal
+from multiprocessing.dummy import Process
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def cart_contents(request):
 
@@ -7,6 +10,17 @@ def cart_contents(request):
     total = 0
     product_count = 0
     grand_total = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity  * product.price
+        product_count  += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = settings.STANDARD_DELIVERY_COST
