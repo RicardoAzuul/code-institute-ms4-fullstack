07df030ee2e8bed8a79c99375264b8edfd3db713
Richard Blaauw <richard.blaauw@ogd.nl>
2022-06-21 16:13:13 +0200

    Add Stripe card element with custom CSS

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KkuhiBJzeXq1XU5sEXIBWCGvyVamO8fZLqItjzlEqCJQL7P2zQGQIAEQvH6EiRMnP7GpFg6DICehDSqvmUpd1qH00aBmy7eKl',
+        'client-secret': 'test client secret',
     }
 
     return render(request, template, context)
