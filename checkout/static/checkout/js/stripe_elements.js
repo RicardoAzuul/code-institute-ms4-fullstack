d9d08dc6c299e094e81e97469bd4a116323642c9
Richard Blaauw <richard.blaauw@ogd.nl>
2022-06-21 20:01:17 +0200

    Add Stripe functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -29,3 +29,95 @@ var card = elements.create('card', {
     style: style
 });
 card.mount('#card-element');
+
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+// On submit, we disable submit functionality so the user can't submit multiple times and causes errors or double charging
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    card.update({
+        'disabled': true
+    });
+    $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
+
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    }
+    var url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function() {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                    address: {
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        city: $.trim(form.town_or_city.value),
+                        country: $.trim(form.country.value),
+                        state: $.trim(form.county.value),
+                    }
+                }
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    postal_code: $.trim(form.postcode.value),
+                    state: $.trim(form.county.value),
+                }
+            },
+        }).then(function (result) {
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(errorDiv).html(html);
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                card.update({
+                    'disabled': false
+                });
+                $('#submit-button').attr('disabled', false);
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
+            }
+        });
+    }).fail(function() {
+        location.reload();
+    })
+});
--- checkout/views.py
+++ checkout/views.py
@@ -1,23 +1,45 @@
+from locale import currency
 from django.shortcuts import redirect, render, reverse
+from django.conf import settings
 
 from checkout.forms import OrderForm
 from django.contrib import messages
 
 from .forms import OrderForm
+from cart.contexts import cart_contents
+
+import stripe
 
 # Create your views here.
 def checkout(request):
+    # Test
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY 
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart =  request.session.get('cart', {})
     if not cart:
         messages.error(request, "Your cart is empty")
         return redirect(reverse('products'))
+    
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key missing.')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KkuhiBJzeXq1XU5sEXIBWCGvyVamO8fZLqItjzlEqCJQL7P2zQGQIAEQvH6EiRMnP7GpFg6DICehDSqvmUpd1qH00aBmy7eKl',
-        'client-secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client-secret': intent.client_secret,
     }
 
     return render(request, template, context)
