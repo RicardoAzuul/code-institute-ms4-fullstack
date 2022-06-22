01c74ddbe0add3cee5f1e107fe08d8b205e65d01
Richard Blaauw <richard.blaauw@ogd.nl>
2022-06-22 20:58:46 +0200

    Add add product functionality: template, view, url. Customize template with CSS

--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
-from multiprocessing import context
-from unicodedata import category
 from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Product_Category 
 from django.conf import settings
+from .forms import ProductForm
+
 
 # Create your views here.
 
@@ -76,3 +76,13 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+def add_product(request):
+    """Add a product to the store"""
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
