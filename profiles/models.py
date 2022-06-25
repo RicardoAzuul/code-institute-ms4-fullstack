56b64fc105e8ff207ca467207d721e1cced74000
Richard Blaauw <richard.blaauw@ogd.nl>
2022-06-25 20:57:02 +0200

    Temporarily enable that profiles are always created

--- profiles/models.py
+++ profiles/models.py
@@ -28,6 +28,8 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     Create or update user profile
     """
-    if created:
-        UserProfile.objects.create(user=instance)
-    instance.userprofile.save()
+    # if created:
+    #     UserProfile.objects.create(user=instance)
+    # instance.userprofile.save()
+
+    UserProfile.objects.create(user=instance)
